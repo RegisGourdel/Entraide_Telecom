d2f534a4600972ec4c83dc28afae74bbe8997937
regis <gourdel.regis@gmail.com>
2017-03-28 01:29:54 +0200

    Ajouts en MACS205

--- MACS205/TD-ED2.R
+++ MACS205/TD-ED2.R
@@ -18,25 +18,30 @@ N(1, 0.2, 1000)
 #' 
 #' On définit les paramètres pour toute la suite du problème :
 #' 
-pars = c(rs = 0.15,
+parms = c(rs = 0.15,
          Ms = 800,
+         ds = 1,
          rc = 0.18,
          Mc = 1200,
-         ds = 1,
          dc = 3)
 
-aux_gompertz = function(r,M,N,n) { r * N * log(M/max(1, N + n)) }
+Nini  = c(Ns = 1000, Nc = 10)
 
-gompertz_competition = function(t, N, params) {
-  ds = aux_gompertz(params[1], params[2], N[1], N[2])
-  dc = aux_gompertz(params[3], params[4], N[2], N[1])
-  return( list(c(ds,dc)) )
+times = seq(0, 150, length.out = 1000)
+
+#' Ainsi que la fonction qui donne la dérivée:
+
+gompertz_competition = function(t,N,params){
+    with(as.list(c(N,parms)), {
+        dNs = rs * N[1] * log(Ms / max(1,N[1] + N[2]))
+        dNc = rc * N[2] * log(Mc / max(1,N[1] + N[2]))
+        return( list(c(dNs,dNc)) )
+    })
 }
 
-Nini  = c(Ns = 1000, Nc = 10)
-times = seq(0, 200, by = 1)
+#' et l'on fait appel à ``ode``:
 
-out   = ode(Nini, times, gompertz_competition, pars)
+out = ode(Nini, times, gompertz_competition, pars)
 summary(out)
 plot(out)
 
@@ -48,7 +53,7 @@ euler = function(yini, times, func, pars) {
   y1 = c(yini[1])
   y2 = c(yini[2])
   for (i in 2:n) {
-    d  = unlist(func(times[i-1], c(y1[i-1], y2[i-1]), pars))
+    d  = unlist( func(times[i-1], c(y1[i-1], y2[i-1]), pars) )
     dt = times[i] - times[i-1]
     y1 = c(y1, y1[i-1] + dt*d[1])
     y2 = c(y2, y2[i-1] + dt*d[2])
@@ -61,7 +66,7 @@ NsEuler = outEuler[1,]
 NcEuler = outEuler[2,]
 
 plot(times, NsEuler, type = 'l', col = "blue", ylab = "Population de cellules saines")
-plot(times, NcEuler, type = 'l', col = "orange", ylab = "Population de cellules cancéreuse")
+lines(times, NcEuler, type = 'l', col = "orange", ylab = "Population de cellules cancéreuse")
 
 #' **Question 6**
 #' 
@@ -72,21 +77,35 @@ plot(times, NcEuler, type = 'l', col = "orange", ylab = "Population de cellules 
 #' 
 
 injection = function(t) {
-    if (t == 30 || t == 31 || t == 55 || t == 56 || t == 80 || t == 81)
+    if ((t >= 30 && t < 31) || (t >= 55 && t < 56) || (t >= 80 && t < 81))
         return(1)
     else
         return(0)
 }
 
 gompertz_chimiotherapie = function(t, N, params) {
-    temps = aux_gompertz(params[1], params[2], N[1], N[2]) - ds * N[1] * injection(t)
-    tempc = aux_gompertz(params[3], params[4], N[2], N[1]) - dc * N[2] * injection(t)
-    return( list(c(temps,tempc)) )
+    with(as.list(c(N,parms)), {
+        dNs = rs * N[1] * log(Ms / max(1,N[1] + N[2])) - ds * N[1] * injection(t)
+        dNc = rc * N[2] * log(Mc / max(1,N[1] + N[2])) - dc * N[2] * injection(t)
+        return( list(c(dNs,dNc)) )
+    })
 }
 
-outChimio = euler(c(1000,10), times, gompertz_chimiotherapie, pars)
-NsChimio = outChimio[1,]
-NcChimio = outChimio[2,]
+plotChimio = function(pas) {
+    times = seq(0, 130, by = pas)
+    outChimio = euler(Nini, times, gompertz_chimiotherapie, pars)
+    NsChimio = outChimio[1,]
+    NcChimio = outChimio[2,]
+    c_range = range(0, NsChimio, NcChimio)
+    plot(times, NsChimio, type = 'l', col = "blue", ylim = c_range)
+    lines(times, NcChimio, type = 'l', col = "orange")
+}
 
-plot(times, NsChimio, type = 'l', col = "blue", ylab = "Population de cellules saines")
-plot(times, NcChimio, type = 'l', col = "orange", ylab = "Population de cellules cancéreuse")
+#' On teste alors la modélisation de la chimio en affinant le pas:
+#' 
+plotChimio(1)
+plotChimio(0.5)
+plotChimio(0.1)
+plotChimio(0.01)
+#plotChimio(0.001)
+
